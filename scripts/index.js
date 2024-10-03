0010042cad5a6c8e3b1510125ff2f060b587ca5d
Eduardo Henrique <itsduzao@outlook.com>
2024-10-03 16:33:33 -0300

    remove unused import

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 import { router } from './router/index.js';
 import { fibonacciHandler } from './fibonacci/fibonacciHandler.js';
-import { containsLetterHandler } from './containsLetter/containsLetterHandler.js';
 import { sumValuesUpToLimitHandler } from './sumValuesUpToLimit/sumValuesUpToLimitHandler.js';
 
 document.addEventListener('DOMContentLoaded', () => {
