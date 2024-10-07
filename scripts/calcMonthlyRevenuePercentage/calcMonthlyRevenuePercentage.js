c2483ac3a5f5a56e1b973318da5e7272b4682ef4
Eduardo Henrique <itsduzao@outlook.com>
2024-10-07 18:29:25 -0300

    Refactor calcMonthlyRevenuePercentage to receive and return an array of objects

--- scripts/calcMonthlyRevenuePercentage/calcMonthlyRevenuePercentage.js
+++ scripts/calcMonthlyRevenuePercentage/calcMonthlyRevenuePercentage.js
@@ -1,9 +1,13 @@
 export function calcMonthlyRevenuePercentage(data) {
-  const RevenueData = data;
-  const total = Object.values(RevenueData).reduce((acc, curr) => acc + Number.parseFloat(curr), 0);
-  const RevenuePercentage = {};
-  for (const key in RevenueData) {
-    RevenuePercentage[key] = `${((Number.parseFloat(RevenueData[key]) / total) * 100).toFixed(2)}%`;
+  const revenueData = data
+  const total = revenueData.map((item) => Number.parseFloat(item.porcentagem)).reduce((acc, curr) => acc + curr, 0)
+  const revenuePercentage = []
+  // adicionar ao revenuePercentage o estado e a porcentagem de cada estado em relação ao total
+  for (const item of revenueData) {
+    revenuePercentage.push({
+      estado: item.estado,
+      porcentagem: `${((Number.parseFloat(item.porcentagem) / total) * 100).toFixed(2)}%`
+    })
   }
-  return RevenuePercentage;
+  return revenuePercentage;
 }
