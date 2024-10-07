7df7e05be49de954b5da856414db43e99aefe8ca
Eduardo Henrique <itsduzao@outlook.com>
2024-10-07 18:30:41 -0300

    Refactor populateHTMLTable to improve readability and maintainability

--- scripts/utils/populateHTMLTable.js
+++ scripts/utils/populateHTMLTable.js
@@ -1,11 +1,15 @@
-export function populateHTMLTable(table, data, thead=false) {
-  let rowIndex = thead ? 0 : 1;
-  for (const [key, value] of Object.entries(data)) {
-    if (rowIndex < table.rows.length) {
-      table.rows[rowIndex].cells[0].innerText = key;
-      table.rows[rowIndex].cells[1].innerText = value;
-      rowIndex++;
+export function populateHTMLTable(table, data) {
+  let rowIndex = 0
+
+  data.map((item) => { 
+    if (rowIndex === 0) {
+      table.rows[rowIndex].cells[0].innerText = Object.keys(item)[0].charAt(0).toUpperCase() + Object.keys(item)[0].slice(1)
+      table.rows[rowIndex].cells[1].innerText = Object.keys(item)[1].charAt(0).toUpperCase() + Object.keys(item)[1].slice(1)
     }
-  }
+    table.rows[rowIndex + 1].cells[0].innerText = item[Object.keys(item)[0]]
+    table.rows[rowIndex + 1].cells[1].innerText = item[Object.keys(item)[1]]  
+  rowIndex++
+  })
+
   return table;
 }
