1876d0f51dea7b20a5b52e227c71fabed32a1af7
Eduardo Henrique <itsduzao@outlook.com>
2024-10-08 10:21:27 -0300

    Refactor calcDailyRevenueHandler.js to add tables with captions for highest revenue, lowest revenue and days above average revenue

--- scripts/calcDailyRevenue/calcDailyRevenueHandler.js
+++ scripts/calcDailyRevenue/calcDailyRevenueHandler.js
@@ -7,17 +7,26 @@ import { populateHTMLTable } from '../utils/populateHTMLTable.js';
 export async function calcDailyRevenueHandler() {
   const output = document.querySelector('#calculateDailyRevenueOutput')
   const data = await fetchData()
-  const { daysAboveAverage, highestRevenue, lowestRevenue } = calcDailyRevenue(data)
   if (!output.hasChildNodes()) {
-    let highestRevenueTable = generateHTMLTable({ rows: 2, cols: Object.keys(highestRevenue).length })
-    highestRevenueTable.rows[0].cells[0].innerText = 'Dia'
-    highestRevenueTable.rows[0].cells[1].innerText = 'Receita'
+    const { daysAboveAverage, highestRevenue, lowestRevenue } = calcDailyRevenue(data)
+    let highestRevenueTable = generateHTMLTable({ rows: ([highestRevenue].length + 1), cols: Object.keys(highestRevenue).length })
     highestRevenueTable.insertAdjacentHTML('afterbegin', '<caption>Dia com maior receita</caption>')
     highestRevenueTable.classList.add('table')
-    highestRevenueTable = populateHTMLTable(highestRevenueTable, highestRevenue)
+    highestRevenueTable = populateHTMLTable(highestRevenueTable, [highestRevenue])
 
+    let lowestRevenueTable = generateHTMLTable({ rows: ([lowestRevenue].length + 1), cols: Object.keys(lowestRevenue).length })
+    lowestRevenueTable.insertAdjacentHTML('afterbegin', '<caption>Dia com menor receita</caption>')
+    lowestRevenueTable.classList.add('table')
+    lowestRevenueTable = populateHTMLTable(lowestRevenueTable, [lowestRevenue])
+
+    let daysAboveAverageTable = generateHTMLTable({ rows: (daysAboveAverage.length + 1), cols: Object.keys(daysAboveAverage[0]).length })
+    daysAboveAverageTable.insertAdjacentHTML('afterbegin', '<caption>Dias com receita acima da média</caption>')
+    daysAboveAverageTable.classList.add('table')
+    daysAboveAverageTable = populateHTMLTable(daysAboveAverageTable, daysAboveAverage)
 
     output.appendChild(highestRevenueTable)
+    output.appendChild(lowestRevenueTable)
+    output.appendChild(daysAboveAverageTable)
   }
   showElement(output)
 }
